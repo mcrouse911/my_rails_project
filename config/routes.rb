33496399cd9de255d329da88df028272220590d7
Michael Crouse <mcrouse911@gmail.com>
2017-06-26 19:59:59 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'welcome#index'
+
+  get 'about_us' => 'welcome#about' 
+
+  get 'contact' => 'welcome#contact'
+
+  get 'home' => 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
